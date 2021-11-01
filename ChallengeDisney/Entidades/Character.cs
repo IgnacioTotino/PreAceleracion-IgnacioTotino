db7f01377e46157a94c3ab382e846434b569db18
Ignacio Tomas Totino <69109625+IgnacioTotino@users.noreply.github.com>
2021-11-01 07:07:08 -0300

    Agregando lo primero trabajado en la API Disney
    Al pasar la semanas va a ser actualizado

---
+++ ChallengeDisney/Entidades/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeDisney.Entidades
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        
+        public string Image { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }        //TODO: cambiar el tipo de edad a string y hacer una migration 
+
+        public int Weight { get; set; }
+
+        public string History { get; set; }
+
+        public string MoviesAssociated { get; set; }
+
+        public Movie Movies { get; set; }
+    }
+}
---
+++ ChallengeDisney/Entidades/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeDisney.Entidades
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Image { set; get; }
+
+        public string Title { get; set; }
+
+        public string CreationDate { get; set; } //cambiar a DateTime
+
+        public int  Qualification { get; set; }
+
+        public string CharacterAssociated { get; set; }
+
+        //Relaciones
+
+        public ICollection<Character> Characters { get; set; }
+
+        public Genre Genres { get; set; }
+    }
+}
